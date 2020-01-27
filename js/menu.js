90600c4a5abc33e6c443dea7b16ee4a9a9366360
daw <daw>
2020-01-27 19:51:56 +0100

    cambio en la forma del header

--- js/menu.js
+++ js/menu.js
@@ -11,9 +11,10 @@ var menuVisible = false;
 
 function cargarEventos() {
   addListener(document.getElementById('icono_menu'), 'click', desplegarMenu);
-
+  var intervaloAnimacionTitulo = setInterval(animarTitulo, 750);
 }
 
+//funcion que cambia la visibilidad del elemento que contiene las secciones
 function desplegarMenu() {
   let contenedorMenu = document.getElementById('contenedor_menus');
   let cabecera = document.getElementById('cabecera');
@@ -24,4 +25,14 @@ function desplegarMenu() {
     contenedorMenu.style.display = 'table-row';
   }
   menuVisible = !menuVisible;
+}
+
+//funcion que hace aparecer el caracter '_' de forma intermitente
+function animarTitulo() {
+  let caracterFinalTitulo = document.getElementById('animacion_titulo');
+  if (caracterFinalTitulo.innerHTML == '') {
+    caracterFinalTitulo.innerHTML = '_';
+  } else if (caracterFinalTitulo.innerHTML == '_') {
+    caracterFinalTitulo.innerHTML = '';
+  }
 }
