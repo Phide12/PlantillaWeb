85ea12402b7f71ea56da71987eb7785985af60d7
casa <casa@dev>
2020-02-08 23:42:23 +0100

    editada parte de conocenos

--- js/animacionScroll.js
+++ js/animacionScroll.js
@@ -2,21 +2,21 @@
  * Carga las funcionalidades que se aplicaran a todas las secciones.
  */
 function cargarAnimacionesScroll() {
-  console.log(document.getElementsByClassName('contenido_parrafo3'));
   
-  listaParrafos = Array.from(document.getElementsByClassName('contenido_parrafo2'));
+  listaParrafos = Array.from(document.getElementsByClassName('contenedor_parrafo_2'));
   addListener(window, 'scroll', cargarEnScroll);
 }
 
 function cargarEnScroll() {
-  
-  for (let i = 0; i < listaParrafos.length; i++) {
-    let parrafo = listaParrafos[i];
-    if (document.documentElement.scrollTop + window.innerHeight > parrafo.offsetTop) {
-      parrafo.classList.add("cargar_imagenes");
-      listaParrafos.splice(parrafo, 1);
-    }
+    for (let i = 0; i < listaParrafos.length; i++) {
+      let parrafo = listaParrafos[i];
+      if (document.documentElement.scrollTop + window.innerHeight > parrafo.offsetTop) {
+        parrafo.classList.add("cargar_imagenes");
+        listaParrafos.splice(parrafo, 1);
+      }
+    
   }
+  
 }
 
 addListener(document, 'DOMContentLoaded', cargarAnimacionesScroll);
