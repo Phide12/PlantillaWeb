0e99c774c923c7e3b58f3b8e5b6c60bfa57d65f5
casa <casa@dev>
2020-02-10 12:07:04 +0100

    finalizado trabajo y añadida guia de estilos

--- js/animacionScroll.js
+++ js/animacionScroll.js
@@ -2,21 +2,20 @@
  * Carga las funcionalidades que se aplicaran a todas las secciones.
  */
 function cargarAnimacionesScroll() {
-  
+
   listaParrafos = Array.from(document.getElementsByClassName('contenedor_parrafo_2'));
   addListener(window, 'scroll', cargarEnScroll);
 }
 
 function cargarEnScroll() {
-    for (let i = 0; i < listaParrafos.length; i++) {
-      let parrafo = listaParrafos[i];
-      if (document.documentElement.scrollTop + window.innerHeight > parrafo.offsetTop) {
-        parrafo.classList.add("cargar_imagenes");
-        listaParrafos.splice(parrafo, 1);
-      }
-    
+  for (let i = 0; i < listaParrafos.length; i++) {
+    let parrafo = listaParrafos[i];
+    if (document.documentElement.scrollTop + window.innerHeight > parrafo.offsetTop) {
+      parrafo.classList.add("cargar_imagenes");
+      listaParrafos.splice(parrafo, 1);
+    }
   }
-  
+
 }
 
 addListener(document, 'DOMContentLoaded', cargarAnimacionesScroll);
